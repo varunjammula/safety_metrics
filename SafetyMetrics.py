0ad94577f60135b831fe33c1ab8bb726a81b6bc9
vjammula <vjammula@asu.edu>
2020-02-09 17:24:44 -0700

    Cleanup for better readability

--- SafetyMetrics.py
+++ SafetyMetrics.py
@@ -2,22 +2,36 @@ class SafetyMetrics:
     def __init__(self):
         print("Loading Safety Metrics")
 
-    def compute_SEV(self):
-        print('Safety Envelope Violation')
-        return
+    def compute_SEV_longitudinal(self, vr, vf, rho, a_accel, a_max_brake, a_min_brake):
+        d_long = ((vr * rho) + 0.5 * a_accel * rho ** 2 + (vr + rho * a_accel ** 2) / (2 * a_min_brake) - vf ** 2 / (
+                    2 * a_max_brake))
+        return d_long
+
+    def compute_SEV_lateral(self, v1, v2, mu, rho, alat_max_brake, alat_min_brake):
+        v1_rho = v1 + rho * alat_max_brake
+        v2_rho = v2 - rho * alat_max_brake
+        d_lat = mu + (((v1 + v1_rho) * rho) / 2 + (v1_rho ** 2 / 2 * alat_min_brake) - ((v2 + v2_rho) * rho) / 2 - (
+                    v2_rho ** 2 / 2 * alat_min_brake))
+        return d_lat
+
+    def compute_SEV_longitudinal_opposite(self, v1, v2, rho, a_min_brake, a_min_brake_correct, a_max_acc):
+        v1_rho = v1 + rho * a_max_acc
+        v2_rho = abs(v2) + rho * a_max_acc
+        d_long_opp = (((v1 + v1_rho) * rho) / 2 + (v1_rho ** 2 / 2 * a_min_brake_correct) + (
+                    (abs(v2) + v2_rho) * rho) / 2 + (v2_rho ** 2 / 2 * a_min_brake))
+        return d_long_opp
 
     def compute_SER(self):
-        print("Safety Envelope Restoration")
         return
 
-    def compute_SEF(self):
+    def compute_SEF_lat(self):
         return
 
     def compute_SECE(self):
         return
 
-    def compute_TTCV(self):
-        return
+    def compute_TTC(self, X_L, X_F, I_L, V_F, V_L):
+        return (X_L - X_F - I_L) / (V_F - V_L)
 
     def compute_MTTC(self):
         return
--- test.py
+++ test.py
@@ -1,14 +1,132 @@
+import math
+import sys
+
 from SafetyMetrics import SafetyMetrics
 import pandas as pd
 from utils.video_frame_counter import get_video_with_frames
+import numpy as np
 
-if __name__=='__main__':
+
+def get_data_by_vehicle_ID(trajectory_data, id):
+    data = trajectory_data.loc[trajectory_data['Vehicle_ID'] == id]
+    return data
+
+
+def get_vehicle_ids(trajectory_data):
+    vehicle_ids = set(trajectory_data.Vehicle_ID)
+    return vehicle_ids
+
+def get_value(df):
+    return df.values[0]
+
+def compute_metrics(trajectory_data, v_following_id, v_leading_id, frame_id, spacing=None):
+    try:
+        vehicle_following = get_data_by_vehicle_ID(trajectory_data, id=v_following_id)
+        vehicle_leading = get_data_by_vehicle_ID(trajectory_data, id=v_leading_id)
+
+        d1_frame = vehicle_following[(vehicle_following['Preceding'] == v_leading_id) & (vehicle_following['Frame_ID'] == frame_id)]
+        d2_frame = vehicle_leading[(vehicle_leading['Following'] == v_following_id) & (vehicle_leading['Frame_ID'] == frame_id)]
+
+        # print(d1_frame)
+        # print(d2_frame)
+
+        velocity_following = get_value(d1_frame.v_Vel)
+        vehicle_length_following = get_value(d1_frame.v_Length)
+        local_y_following = get_value(d1_frame.Local_Y)
+        acc_following = get_value(d1_frame.v_Acc)
+
+        velocity_leading = get_value(d2_frame.v_Vel)
+        vehicle_length_leading = get_value(d2_frame.v_Length)
+        local_y_leading = get_value(d2_frame.Local_Y)
+        acc_leading = get_value(d2_frame.v_Acc)
+        print('acc ', end='')
+        print(acc_following, acc_leading)
+        #print(local_y_leading - local_y_following - vehicle_length_following)
+        print('vel ', end='')
+        print(velocity_following, velocity_leading)
+        # print(d1_frame)
+        sev_long = sm.compute_SEV_longitudinal(velocity_following, vf=velocity_leading, rho=2, a_accel=9.13, a_max_brake=-10.11, a_min_brake=-5.23)
+        print('SEV: {}'.format(sev_long))
+
+        ttc = sm.compute_TTC(local_y_leading, local_y_following, vehicle_length_following, velocity_following, velocity_leading)
+        print('TTC: {}'.format(ttc))
+        print('-----')
+    except Exception as e:
+        print(e)
+
+
+def clean_data(trajectory_data):
+    bad_data1 = trajectory_data[trajectory_data['v_Acc'] == 11.2].index
+    # print(bad_data.index)
+    print(len(bad_data1))
+
+    bad_data2 = trajectory_data[trajectory_data['v_Acc'] == -11.2].index
+    # print(bad_data.index)
+    print(len(bad_data2))
+
+    good_count = (len(trajectory_data) - len(bad_data1) - len(bad_data2))
+
+    print('good_count: {}'.format(good_count))
+    print('% good: {}'.format(good_count / len(trajectory_data)))
+
+    trajectory_data.drop(bad_data1, inplace=True)
+    print(len(trajectory_data))
+
+    trajectory_data.drop(bad_data2, inplace=True)
+    print(len(trajectory_data))
+
+    trajectory_data.to_csv('data/trajectories-0400-0415_filtered.csv', index=False)
+
+
+def acc_analysis(trajectory_data):
+    ids = list(get_vehicle_ids(trajectory_data))
+    print(len(ids))
+    sampling_time = 0.1
+    a_comp_min, a_comp_max, a_data_min, a_data_max, a_comp_avg, a_data_avg, a_comp_var, a_data_var = [], [], [], [], [], [], [], []
+    v_id = []
+    # ids = [213]
+    for id in ids:
+        print(id)
+        data = get_data_by_vehicle_ID(trajectory_data, id)
+        velocities = list(data.v_Vel)
+        acc_data = list(data.v_Acc)
+        acc_computed = []
+        for i in range(1, len(velocities)):
+            a = (velocities[i] - velocities[i - 1]) / sampling_time
+            acc_computed.append(a)
+
+            # print('vel_b: {}, vel_a:{}, computed: {}, data: {}, a_before:{}'.format(velocities[i - 1], velocities[i],
+            #                                                                             a, acc_data[i], acc_data[i - 1]))
+        a_comp_min.append(np.nanmin(acc_computed))
+        a_comp_max.append(np.nanmax(acc_computed))
+        a_comp_avg.append(np.nanmean(acc_computed))
+        a_comp_var.append(np.nanvar(acc_computed))
+
+        a_data_min.append(np.nanmin(acc_data[1:]))
+        a_data_max.append(np.nanmax(acc_data[1:]))
+        a_data_avg.append(np.nanmean(acc_data[1:]))
+        a_data_var.append(np.nanvar(acc_data[1:]))
+        v_id.append(id)
+
+    df= pd.DataFrame({'vehicle_ID' : v_id, 'a_comp_min': a_comp_min, 'a_comp_max': a_comp_max, 'a_comp_avg': a_comp_avg, 'a_comp_var': a_comp_var,
+         'a_data_min': a_data_min, 'a_data_max': a_data_max, 'a_data_avg': a_data_avg, 'a_data_var': a_data_var})
+
+    df.to_csv('acc_analysis.csv', index=False)
+
+
+
+if __name__ == '__main__':
     sm = SafetyMetrics()
 
     trajectory_data = pd.read_csv('data/trajectories-0400-0415.csv', low_memory=False)
-    # print(trajectory_data[1])
-    select_price = trajectory_data.loc[trajectory_data['Vehicle_ID'] == 1]
-    print(select_price)
+    print(len(trajectory_data))
 
-    get_video_with_frames('data/nb-camera5-0400pm-0415pm-processed.avi', 'data/output.avi')
+    compute_metrics(trajectory_data, 250, 243, frame_id=1301)
+    compute_metrics(trajectory_data, 242, 231, frame_id=1301)
+    compute_metrics(trajectory_data, 213, 210, frame_id=1301)
+
+    # val = sm.compute_SEV_longitudinal(vr, vf=vf, rho=0.8, a_accel=8.42, a_max_brake=-4.31, a_min_brake=-11.1)
+    # val = sm.compute_SEV_longitudinal(vr, vf=vf, rho=0.8, a_accel=6.2, a_max_brake=-4.42, a_min_brake=-10.84)
+
+
 
